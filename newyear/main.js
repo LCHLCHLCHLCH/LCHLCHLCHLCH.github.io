50a8a07cb104e1c38cc65f85ef3746b33524296e
LCHLCHLCHLCH <105085959+LCHLCHLCHLCH@users.noreply.github.com>
2022-12-31 23:30:03 +0800

    Add files via upload

--- newyear/main.js
+++ newyear/main.js
@@ -8,13 +8,13 @@ function showcontent(){
     var wor7 = document.getElementById("word7").value;
     var wor8 = document.getElementById("word8").value;
     var wor9 = document.getElementById("word9").value;
-        if(wor1=="treat" && wor2=="people" && wor3=="sincerely" && wor4=="be" && wor5=="grateful" && wor6=="to" && wor7=="what" && wor8=="you" && wor9=="have"){
+        if((wor1=="treat" ||wor1=="Treat")&& wor2=="people" && wor3=="sincerely" && wor4=="be" && wor5=="grateful" && wor6=="to" && wor7=="what" && wor8=="you" && (wor9=="have"||wor9=="have.")){
             frame.src="./res/XS.html"
     }
-        if(wor1=="with" && wor2=="persistence" && wor3=="and" && wor4=="determination" && wor5=="we" && wor6=="can" && wor7=="persue" && wor8=="our" && wor9=="dream"){
+        if((wor1=="with"||wor1=="With" )&& wor2=="persistence" && wor3=="and" && wor4=="determination" && wor5=="we" && wor6=="can" && wor7=="persue" && wor8=="our" && (wor9=="dream"||wor9=="dream.")){
         frame.src="./res/LJQ.html"
     }
-    if(wor1=="never" && wor2=="give" && wor3=="up" && wor4=="no" && wor5=="matter" && wor6=="what" && wor7=="difficulty" && wor8=="you" && wor9=="meet"){
+    if((wor1=="never"||wor1=="Never" )&& wor2=="give" && wor3=="up" && wor4=="no" && wor5=="matter" && wor6=="what" && wor7=="difficulty" && wor8=="you" && (wor9=="meet"||wor9=="meet.")){
         frame.src="./res/HPY.html"
     }
 }
